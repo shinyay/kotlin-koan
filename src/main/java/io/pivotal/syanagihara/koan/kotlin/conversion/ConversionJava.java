b3cea185adbb7ba971bcac062cd70df0cb079643
shinyay <shinya.com@gmail.com>
2018-09-09 20:15:01 +0900

    Add Conversion sample from Java to Kotlin

---
+++ src/main/java/io/pivotal/syanagihara/koan/kotlin/conversion/ConversionJava.java
@@ -0,0 +1,22 @@
+package io.pivotal.syanagihara.koan.kotlin.conversion;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class ConversionJava {
+
+    public String toJSON(Collection<Integer> collection) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Iterator<Integer> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            sb.append(element);
+            if (iterator.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/kotlin/io/pivotal/syanagihara/koan/kotlin/conversion/ConversionKotlin.kt
@@ -0,0 +1,19 @@
+package io.pivotal.syanagihara.koan.kotlin.conversion
+
+class Conversion {
+
+    fun toJSON(collection: Collection<Int>): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        val iterator = collection.iterator()
+        while (iterator.hasNext()) {
+            val element = iterator.next()
+            sb.append(element)
+            if (iterator.hasNext()) {
+                sb.append(", ")
+            }
+        }
+        sb.append("]")
+        return sb.toString()
+    }
+}
